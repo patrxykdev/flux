bb94b0d80d269b5bbed08d001fdd63174bd05ffa
patryk <patryk@Patryks-MacBook-Air.local>
2025-08-29 13:23:51 +0100

    nginx

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include 
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -19,13 +18,6 @@ urlpatterns = [
     # This endpoint will be used to get a new access token once it expires
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
-
-# Serve React app for all other routes (fallback for client-side routing)
-if not settings.DEBUG:
-    urlpatterns += [
-        path('', TemplateView.as_view(template_name='index.html')),
-        path('<path:path>', TemplateView.as_view(template_name='index.html')),
-    ]
 
 # Serve static files in development
 if settings.DEBUG:
